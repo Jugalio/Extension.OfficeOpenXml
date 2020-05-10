c9a11f8131e173dc62eae964269a408a2c1c3906
Jugalio <33733301+Jugalio@users.noreply.github.com>
2020-05-10 16:49:57 +0200

    Initial commit

---
+++ src/Extension.OfficeOpenXml/Excel/ExcelFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension.OfficeOpenXml.Excel
+{
+    public class ExcelFile
+    {
+
+        public List<ExcelSheet> Sheets;
+
+    }
+}
---
+++ src/Extension.OfficeOpenXml/Excel/ExcelRow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension.OfficeOpenXml.Excel
+{
+    public class ExcelRow
+    {
+
+        public List<ExcelCell> Cells;
+
+    }
+}
